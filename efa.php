8b521aaa408c3e22d9f46e44f09e5cac4f45a1f8
AG <americo.guerreiro@efacec.com>
2023-07-14 16:59:40 +0100

    atualização path common

--- efa.php
+++ efa.php
@@ -1,5 +1,10 @@
 <?PHP
 //	2013-10-10	Luis Cruz		UTF-8 version
-set_include_path(get_include_path() . PATH_SEPARATOR . '../common');
-require_once('../common/efa.php');
+//set_include_path(get_include_path() . PATH_SEPARATOR . '../common');
+//require_once('../common/efa.php');
+
+//	2023-07-07	AG		UTF-8 version
+set_include_path(get_include_path() . PATH_SEPARATOR . '../SynergyNetCommon/common');
+require_once('../SynergyNetCommon/common/efa.php');
+
 ?>
